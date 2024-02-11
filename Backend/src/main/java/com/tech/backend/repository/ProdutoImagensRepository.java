3ac4e08121813241cdc3813ed03df0abb4a13652
andressaribeiroo <andressaclecia.ribeiro@gmail.com>
2024-02-11 20:20:14 -0300

    feat: crud ProdutoImagens

---
+++ Backend/src/main/java/com/tech/backend/repository/ProdutoImagensRepository.java
@@ -0,0 +1,7 @@
+package com.tech.backend.repository;
+
+import com.tech.backend.entity.ProdutoImagens;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProdutoImagensRepository extends JpaRepository<ProdutoImagens, Long> {
+}
